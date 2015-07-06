360ccc8419d6d2ecf99dbec346405bcdf1e77588
Eric Chu <eric@ericchu.net>
2015-07-06 19:17:28 -0400

    Initial commit

---
+++ app/src/main/java/net/ericchu/stalkmarket/StalkMarketActivity.java
@@ -0,0 +1,14 @@
+package net.ericchu.stalkmarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class StalkMarketActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stalk_market);
+    }
+}
